7687bf8f84c081c3adb4dfab26f1e0065f8d7a72
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-01-29 23:50:31 -0500

    Added inital frontend code

---
+++ src/foodmobile-frontend/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Index from './_components/index'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Index>
+        <Text >I ant done anything yet</Text>
+        <Text>Come bac in year?</Text>
+        <Text>tHAnk</Text>
+        <Text>WeLck</Text>
+      </Index>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
---
+++ src/foodmobile-frontend/packages.md
@@ -0,0 +1,6 @@
+# Packages
+
+This is my attempt to try and remember why I added certain packages.
+
+
+## List
