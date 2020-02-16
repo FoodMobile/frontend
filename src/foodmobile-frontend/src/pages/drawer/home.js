800803224c587b4fc8f6ef8251f83795c13db96b
ShahNafisRafique <sarafiqu@uncg.edu>
2020-02-15 22:26:08 -0500

    drawer home

---
+++ src/foodmobile-frontend/src/pages/drawer/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+import { Button,Text } from 'react-native-paper';
+import PreferencesContext from '../context/context';
+
+
+//Simple page that adds a tab that allows to change theme
+export default function DrawerHome(props) {
+    return (
+        <TestTab/>
+    )
+}
