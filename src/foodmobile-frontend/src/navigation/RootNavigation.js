528c708b80609fc13ad216bc0dc4435f72bbbfa9
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-02-21 22:22:55 -0500

    removed drawer ,its all tabs now

--- src/foodmobile-frontend/src/navigation/RootNavigation.js
+++ src/foodmobile-frontend/src/navigation/RootNavigation.js
@@ -9,6 +9,99 @@ import { useTheme } from 'react-native-paper';
 
 const Drawer = createDrawerNavigator();
 
+//
+import { List, Checkbox,Button  } from 'react-native-paper';
+import { createStackNavigator } from '@react-navigation/stack';
+import {
+Text
+} from 'react-native-paper';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+function MyOrder() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>My Orders</Text>
+      </View>
+    );
+}
+  
+function MyMaps() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Map</Text>
+      </View>
+    );
+}
+
+function Fav() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Favorite</Text>
+      </View>
+    );
+}
+
+function setHome({navigation}) {
+    return (
+        <List.Section title="General Settings">
+            <List.Accordion
+            title="Settings1"
+            left={props => <List.Icon {...props} icon="folder" />}
+            >
+            <List.Item title="Set1" onPress={() =>navigation.navigate('Set1')}/>
+
+            <List.Item title="Second item" onPress={() =>navigation.navigate('MyOrders')}/>
+            </List.Accordion>
+
+          
+        </List.Section>
+    );
+}
+
+function set1() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>set1</Text>
+      </View>
+    );
+}
+
+
+class Settings extends React.Component {
+    state = {
+        expanded: true
+    }
+    
+    _handlePress = () =>
+        this.setState({
+        expanded: !this.state.expanded
+    });
+    
+    render() {
+        return (
+            <>
+            
+            <Stack.Navigator>
+                <Stack.Screen name="SetHome" component={setHome} >
+                </Stack.Screen>
+                
+                <Stack.Screen name="Set1" component={set1} />    
+            </Stack.Navigator>
+
+           
+           
+            </>
+        );
+    }
+    
+    
+}
+  
+const Tab = createMaterialBottomTabNavigator();
+const Stack = createStackNavigator();
+
+
+//
+
 //Pages
 import MainStack from './stack/mainStack'
 
@@ -20,13 +113,12 @@ export function RootNavigation() {
     const navigationTheme = theme.dark ? DarkTheme : DefaultTheme;
     return (
         <NavigationContainer theme={navigationTheme}>
-            <Drawer.Navigator  
-                drawerContent={props => <DrawerContent {...props} />}  
-                edgeWidth = {3000} 
-                minSwipeDistance = {0}
-            >
-                <Drawer.Screen name="MainDrawer" component={MainStack} />
-            </Drawer.Navigator>
+             <Tab.Navigator labeled = {true} shifting = {false}>
+                <Tab.Screen name="MyOrders" component={MyOrder} />
+                <Tab.Screen name="Map" component={MyMaps} />
+                <Tab.Screen name="Fav" component={Fav} />
+                <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 }
