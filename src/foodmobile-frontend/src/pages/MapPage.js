f8f9e65e0a39777641bc5b06dac86526debfb2a4
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-04-08 20:33:14 -0400

    removed redundent files and removed componentWillMount warning

--- src/foodmobile-frontend/src/pages/MapPage.js
+++ src/foodmobile-frontend/src/pages/MapPage.js
@@ -1,67 +1,79 @@
-import * as React from 'react';
-import { StyleSheet, Dimensions, View, AsyncStorage,RefreshControl,ScrollView } from 'react-native';
+import React, { Component,useState } from 'react';
+import { Platform, View, StyleSheet } from 'react-native';
 import { Button, Text } from 'react-native-paper';
-import { createStackNavigator } from '@react-navigation/stack';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
-import { Camera } from 'expo-camera';
-import AppbarWrapper from '../components/appBar/appBarWrapper'
-import ScreenNames from '../screenNames'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const Tab = createMaterialTopTabNavigator();
-// import GetCameraPage from './testCameraPage' 
-// import GetMapPage from './testMapPage'
-import ShowLocation from './testShowLoc'
-function MapHome() {
-  return(
-    <View style={styles.container}>
-    </View>
-  )
+import GetMapPage from './getMap'
+
+
+export default class MapPage extends Component {
+    state = {
+      location: null,
+      errorMessage: null,
+      isGetting:'maybe'
+    };
+  
+    componentDidMount() {
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+        this.setState({
+          errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+        });
+      } else {
+        this._getLocationAsync();
+      }
+    }
+  
+    _getLocationAsync = async () => {
+        this.setState({ isGetting:'getting...' });
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status !== 'granted') {
+        this.setState({
+          errorMessage: 'Permission to access location was denied',
+        });
+      }
+  
+      let location = await Location.getCurrentPositionAsync({});
+      this.setState({ location });
+      this.setState({ isGetting:'done' });
+    };
+  
+    render() {
+      let text = 'Waiting..';
+      if (this.state.errorMessage) {
+        text = this.state.errorMessage;
+      } else if (this.state.location) {
+        text = JSON.stringify(this.state.location);
+      }
+  
+      return (
+        <View style={styles.container}>
+          {/* <Text style={styles.paragraph}>{text}</Text>
+          <Button onPress={()=>{this._getLocationAsync()}}>
+              Update
+          </Button>
+          <Text style={styles.paragraph}>{this.state.isGetting}</Text> */}
+          {(this.state.isGetting == 'done') && <GetMapPage location = {this.state.location}/>}
+          <Button onPress={()=>{this._getLocationAsync()}}>
+              Update:{this.state.isGetting}
+          </Button>
+        </View>
+      );
+    }
 }
-
-// function mapCam() {
-//   return(
-//     <GetCameraPage/>
-//   )
-// }
-
-// function mapLoc() {
-//   return(
-//     <GetMapPage/>
-//   )
-// }
-
-export default function MapPage({ navigation }) {
-    const {map} =  ScreenNames.stackPages
-    return (
-      // <Tab.Navigator>
-      //    <Tab.Screen name="Map Home" component={mapHome}/>
-      //    <Tab.Screen name="Map Location" component={ShowLocation}/>
-      //    <Tab.Screen name="Map Cam" component={mapCam}/>
-      //    <Tab.Screen name="Map Loc" component={mapLoc}/>
-      // </Tab.Navigator>
-      <ShowLocation/>
-    );
-}
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     flexDirection: 'column',
-//     backgroundColor: '#000',
-//   },
-//   mapContainer: {
-//     flex: 3,
-//     // flexWrap: 'wrap',
-//     backgroundColor: '#ffab00', // orange
-//     alignItems: 'center',
-//     justifyContent:'center',
-//   },
-//   truckListContainer: {
-//     flex: 1,
-//     flexWrap: 'wrap',
-//     backgroundColor: '#ffab00', //greyish color
-//   },
-// });
-
-
+  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
